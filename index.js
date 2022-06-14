d0f5dfc81fbcefc1575205a55bbc7448c339a0ef
Kristina <98588919+KristinaKishta@users.noreply.github.com>
2022-06-14 06:26:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ let taxOver450 = (taxedSalary_1 - 450) * (10 / 100)
 let totalTax = taxUnder80 + taxUnder250 + taxUnder450 + taxOver450
 console.log(totalTax)
 let takeHomePay_1 = taxedSalary_1 - totalTax
-console.log("The total take home pay is: " + takeHomePay_1)
+console.log("The total home pay is: " + takeHomePay_1)
 
 //Salary 2
 const grossSalary_2 = 200
 let pensionContribution_2 = grossSalary_2 * (5 / 100);
 let taxedSalary_2 = grossSalary_2 - pensionContribution_2
 let takeHomePay_2 = taxedSalary_2 - totalTax
-console.log("The total take home pay is:" + takeHomePay_2)
+console.log("The total home pay is:" + takeHomePay_2)
